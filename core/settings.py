e54b1c66d7a52508f607cb2fc3a20ae5cf050070
felipemadeit <felipemadeit@gmail.com>
2024-08-14 13:59:22 -0500

    Se agrego whitenoise a installed apps

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-2024-production-84bd.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1','portfolio-2024-production-84bd.up.railway.app']
 
 
 # Application definition
@@ -124,4 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-CSRF_TRUSTED_ORIGINS = ['https://*', 'portfolio-2024-production-84bd.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['http://*', 'http://portfolio-2024-production-84bd.up.railway.app']
